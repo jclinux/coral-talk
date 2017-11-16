ed683c00af8e7a19835453de31f54d5b4815141c
Josh Kadis <josh@alleyinteractive.com>
2017-11-15 21:20:01 -0500

    allow env variable to override Express template for embed stream

--- routes/embed/index.js
+++ routes/embed/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const SettingsService = require('../../services/settings');
 
+const viewPath = process.env.EMBED_VIEW_OVERRIDE_DIR ?
+  `../${process.env.EMBED_VIEW_OVERRIDE_DIR}/stream`:
+  'embed/stream';
+
 router.use('/:embed', async (req, res, next) => {
   switch (req.params.embed) {
   case 'stream': {
     const {customCssUrl} = await SettingsService.retrieve('customCssUrl');
-    return res.render('embed/stream', {customCssUrl});
+    return res.render(viewPath, {customCssUrl});
   }
   }
 
